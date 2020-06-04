760e5883b3311181c2e6160e5cc0d0e34a3731ec
Rebecca Sousa <rebecca_sousa@hotmail.com>
2020-06-04 14:28:11 -0400

    Created hover change color effect

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const container = document.querySelector('.container');
 
+let cell;
 let size = 16;
 
 function makeGrid(size) {
     container.style.setProperty('--columns', size);
     container.style.setProperty('--rows', size);
     for(let i = 0; i < (size * size); i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
+        cell = document.createElement('div');
+        cell.classList.add('contCell');
         container.appendChild(cell);
     }
 }
 
 makeGrid(size);
+
+const cellBlack = document.querySelectorAll('.contCell');
+
+cellBlack.forEach(function(contCell) {
+    contCell.addEventListener('mouseover', function() {
+        //contCell.style.backgroundColor = 'black';
+        paintCell(contCell);
+    })
+})
+
+function paintCell(bla) {
+    bla.style.backgroundColor = 'black';
+}
+
